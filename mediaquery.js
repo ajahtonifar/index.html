36ea7c91d719c516a59ba17be9da7c4af30060eb
Ajah Junior Tonifar <shehazahdbrad@gmail.com>
2026-05-22 21:21:24 -0700

    master

--- mediaquery.js
+++ mediaquery.js
@@ -16,3 +16,45 @@ function turnOff(){
 
     bulb.classList.remove("on");
 }
+
+/* working on the decrement and increment function*/
+
+let count = 0;
+
+const number = document.getElementById("number");
+
+const saveEl = document.getElementById("save");
+
+/* Increment */
+
+function increment() {
+
+    count++;
+
+    number.textContent = count;
+}
+
+/* Decrement */
+
+function decrement() {
+
+    if(count > 0){
+
+        count--;
+
+        number.textContent = count;
+    }
+}
+
+/* Save */
+
+function saveEntry() {
+
+    let savedNumber = count + " - ";
+
+    saveEl.textContent += savedNumber;
+
+    count = 0;
+
+    number.textContent = count;
+}
